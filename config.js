f65273fcfce5f2049c98219c504f380bdc1fba79
Eric <ericoliseum@gmail.com>
2026-03-14 15:11:12 +0100

    claves puestas

--- config.js
+++ config.js
@@ -5,13 +5,13 @@
 //  3. Pega los valores aquí
 // ============================================================
 
-const SUPABASE_URL  = 'https://TU_PROYECTO.supabase.co';   // <-- cambia esto
-const SUPABASE_ANON = 'TU_ANON_KEY';                        // <-- cambia esto
+const SUPABASE_URL = 'https://mgcizeoreaucrxfieqwi.supabase.co';   // <-- cambia esto
+const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1nY2l6ZW9yZWF1Y3J4ZmllcXdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0ODUwODEsImV4cCI6MjA4OTA2MTA4MX0.QEhR2YR6xWty_u0-haHCB8Rnn8TCZgRBcrpr44ovo3Q';                        // <-- cambia esto
 
 // XP que se gana por batalla
-const XP_WIN  = 30;
+const XP_WIN = 30;
 const XP_LOSS = 10;
 
 // XP necesario para subir de nivel
-function xpForLevel(lvl){ return lvl * 100; }
+function xpForLevel(lvl) { return lvl * 100; }
 
